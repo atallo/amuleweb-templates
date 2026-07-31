f76a2c07ed1befc7c16c49e2733f45f2c084e88b
Víctor Sánchez <586093+atallo@users.noreply.github.com>
2026-07-31 22:03:12 +0200

    login: prevent browser caching via no-cache http-equiv meta tags

--- templates/reloaded/login.php
+++ templates/reloaded/login.php
@@ -13,6 +13,9 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8" />
+	<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+	<meta http-equiv="Pragma" content="no-cache" />
+	<meta http-equiv="Expires" content="0" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<title>aMule - Control Panel - Login</title>
 	<link rel="icon" href="favicon.ico" />
